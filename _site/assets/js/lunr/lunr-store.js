709ee9bdbcaa398bea4dc608f89d215057438dee
gudals113 <gudals113@gmail.com>
2021-12-30 19:37:36 +0900

    1
    1

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "Test",
+        "excerpt":" title:  “test용 문서 작성” excerpt: “목록에서 보여? “   categories:     Blog tags:   [Blog, Daily]   toc: true toc_sticky: true   date: 2021-12-30  ","categories": [],
+        "tags": [],
+        "url": "/blog/test/",
+        "teaser": null
+      }]
